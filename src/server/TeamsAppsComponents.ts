0f9b3336f2f2176b9ebdd22ddef69e9e5f823a52
mirko.petrelli <mirko.petrelli@avanade.com>
2023-03-13 15:02:36 +0100

    Conversational bot WIP

--- src/server/TeamsAppsComponents.ts
+++ src/server/TeamsAppsComponents.ts
@@ -1,6 +1,9 @@
 // Components will be added here
 
 export const nonce = {}; // Do not remove!
+
 export * from "./planetBot/planetBot";
 export * from "./MS600TAB_PERSONAL/MS600TAB_PERSONAL";
 export * from "./MS600ConfigTab/MS600ConfigTab";
+
+export * from "./conversationalBot/ConversationalBot";
